0dfe775ce813da0177fae7fcb0cdf6c86e9c0518
이선호 <dltjsgho94@gmail.com>
2021-12-15 23:52:15 +0900

    Add:nav 로그인 modal창 구현

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -1,23 +1,37 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styled from 'styled-components';
 
 function Nav() {
+  const [toggle, setToggle] = useState(false);
+
   return (
     <Container>
-      <Logo>Nanoom로고</Logo>
-      <Gnb>
-        <Search>
-          <i class="fas fa-search" />
-        </Search>
-        <Login>로그인</Login>
-      </Gnb>
+      <Inner>
+        <Logo>Nanoom로고</Logo>
+        <Gnb>
+          <Search>
+            <i className="fas fa-search" />
+          </Search>
+          <Login
+            onClick={() => {
+              setToggle(prev => !prev);
+            }}
+          >
+            로그인
+          </Login>
+        </Gnb>
+      </Inner>
+      <Modal isClickModal={toggle}>
+        <li className="kakao">카카오톡으로 로그인</li>
+      </Modal>
     </Container>
   );
 }
 
 export default Nav;
 
-const Container = styled.div`
+const Container = styled.div``;
+const Inner = styled.div`
   position: sticky;
   top: 0px;
   left: 0px;
@@ -32,11 +46,15 @@ const Container = styled.div`
   transition: all 700ms ease 0s;
   background-color: rgba(255, 255, 255, 0.114);
   backdrop-filter: blur(5px);
+  /* border: 1px solid black; */
 `;
 
-const Logo = styled.div``;
+const Logo = styled.div`
+  border: 1px solid black;
+`;
 
 const Gnb = styled.div`
+  position: relative;
   display: flex;
   justify-content: center;
   align-items: center;
@@ -45,6 +63,7 @@ const Gnb = styled.div`
 
 const Search = styled.div`
   cursor: pointer;
+  font-size: 20px;
 `;
 
 const Login = styled.div`
@@ -54,4 +73,23 @@ const Login = styled.div`
   background: #ff9900;
   color: #fff;
   cursor: pointer;
+`;
+
+const Modal = styled.ul`
+  position: absolute;
+  right: 0;
+  margin-right: 2rem;
+  padding: 2rem;
+  border-radius: 10px;
+  background: #fefefe;
+  box-shadow: 0 4px 20px 0 rgb(0 0 0 / 10%);
+
+  ${props => props.isClickModal && `display:none`};
+
+  .kakao {
+    padding: 15px;
+    background: #fae100;
+    border-radius: 10px;
+    cursor: pointer;
+  }
 `;
